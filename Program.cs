0eb6ad81cc5d2791785c3f885c200aa2bd5f72e1
Camilobarre <barrecami@gmail.com>
2024-10-07 11:07:38 -0500

    feat: Se implementa las variables Env del JWT en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using System.Text;
 using DotNetEnv;
+using FiltroDotnet.Config;
 using FiltroDotnet.Data;
 using FiltroDotnet.Repositories;
 using FiltroDotnet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 Env.Load();  // Carga las variables de entorno desde un archivo .env
 
@@ -29,10 +34,69 @@ builder.Services.AddScoped<IRoomTypeRepository, RoomTypeServices>();
 builder.Services.AddScoped<IBookingRepository, BookingServices>();
 builder.Services.AddScoped<IGuestRepository, GuestServices>();
 
+builder.Services.AddSingleton<Utilities>();
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+        ValidateAudience = false,
+        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY")!))
+    };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = ".NET Module Performance Test",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Camilo Barreneche Escobar",
+            Email = "barrecami@gmail.com",
+        }
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "This performance test seeks to develop a RESTful API that allows a hotel to manage its reservations.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -40,12 +104,22 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", ".NET Test");
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseWelcomePage(new WelcomePageOptions
+{
+    Path = "/"
+});
 
 app.MapControllers();
 
